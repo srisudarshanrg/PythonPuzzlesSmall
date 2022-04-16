2a101ca695fc68a47100984d76b4e8645f7048cb
Sri Sudarshan <srisudarshanrg@gmail.com>
2022-04-16 17:57:17 +0530

    Text-To-Speech

--- Text-To-Speech/Text-To-Speech.py
+++ Text-To-Speech/Text-To-Speech.py
@@ -1,16 +1,4 @@
 from tkinter import *
-from gtts import gTTS
-from plyer import notification
 
 root = Tk()
 root.geometry("500x500")
-root.title("Convert Text to Speech")
-
-button = Button(root, text = "duh")
-button.grid(row=0, column=0, padx=5, pady=5)
-
-message = "me"
-title = "duh"
-notification.notify(title = title,
-                    message = message,
-                    timeout = 10)
