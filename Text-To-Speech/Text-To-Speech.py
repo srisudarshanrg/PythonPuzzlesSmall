40fbfe5e0646c913b6cdefc2a7334bf560d76e9f
Sri Sudarshan <srisudarshanrg@gmail.com>
2022-04-16 18:34:10 +0530

    Text-to-Speech

--- Text-To-Speech/Text-To-Speech.py
+++ Text-To-Speech/Text-To-Speech.py
@@ -1,4 +1,31 @@
 from tkinter import *
+from PIL import Image, ImageTk
+from plyer import notification
+from gtts import gTTS
 
-root = Tk()
-root.geometry("500x500")
+translator = Tk()
+translator.geometry("500x500")
+translator.title("Text-To-Speech Converter")
+translator.resizable(0, 0)
+photo = PhotoImage(file = "F:\Sri\Git\PythonPuzzles\Text-To-Speech\Mic.png")
+translator.iconphoto(False, photo)
+
+class open_audio():
+    def __init__(self):
+        pass
+
+#menu bar 
+menu_bar = Menu(translator)
+translator.configure(menu = menu_bar)
+
+open_menu = Menu(menu_bar, tearoff = False)
+menu_bar.add_cascade(label = "Open", menu = open_menu)
+open_menu.add_command(label = "Open Audio File", command = open_audio())
+open_menu.add_separator()
+open_menu.add_command(label = "Make Entered Text as Text File")
+
+close_menu = Menu(menu_bar, tearoff = False)
+menu_bar.add_cascade(label = "Close", menu = close_menu)
+close_menu.add_command(label = "Close", command = lambda: translator.destroy())
+
+translator.mainloop()
