65939707cd0f05ade3b0dbe17115c82c4d2e9e46
Sri Sudarshan <srisudarshanrg@gmail.com>
2022-04-18 13:50:19 +0530

    Home Screen

--- TextEditor/TextEditor.py
+++ TextEditor/TextEditor.py
@@ -8,18 +8,43 @@ import os
 import time
 
 main = Tk()
-main.geometry("1000x800")
+main.geometry("1000x600")
 main.title("Textobot Text Editor")
+icon = PhotoImage(file = "F:\Sri\Git\PythonPuzzles\TextEditor\Textobot.png")
+main.iconphoto(False, icon)
 
-button_newfile = Button(main, text="Create New File", font=("Rockwell", 20, "bold"), command=lambda: step())
-button_newfile.grid(row=0, column=0, padx=5, pady=5)
+menubar = Menu(main)
+main.configure(menu=menubar)
 
-progressbar_ = ttk.Progressbar(main, orient=HORIZONTAL, length=300, mode='determinate')
-progressbar_.grid(row=1, column=0, padx=5, pady=5)
+file_menu = Menu(menubar, tearoff=False)
+menubar.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New File")
+file_menu.add_command(label="Open File")
+file_menu.add_separator()
+file_menu.add_command(label="Close Window")
 
-def step():
-    for x in range(5):
-        progressbar_['value'] += 20
-        x+=1
+help_menu = Menu(menubar, tearoff=False)
+menubar.add_cascade(label="Help", menu=help_menu)
+help_menu.add_command(label="Search Help")
+help_menu.add_separator()
+help_menu.add_command(label="Web Help")
+
+close_menu = Menu(menubar, tearoff=False)
+menubar.add_cascade(label="Close", menu=close_menu)
+close_menu.add_command(label="Close Window", command=lambda: main.destroy())
+
+image_newfile = ImageTk.PhotoImage(Image.open("F:\\Sri\\Git\\PythonPuzzles\\TextEditor\\New File.png"))
+button_newfile = Button(main, image=image_newfile, relief = RIDGE, borderwidth=4, bg="red")
+button_newfile.grid(row=0, column=0, padx=430, pady=20)
+
+label_newfile = Label(main, text = "Create New File☝", relief = FLAT, font = ("Rockwell", 15, "bold"))
+label_newfile.grid(row=1, column=0, padx=430)
+
+image_openfile = ImageTk.PhotoImage(Image.open("F:\\Sri\\Git\\PythonPuzzles\\TextEditor\\Open File.png"))
+button_openfile = Button(main, image=image_openfile, relief = RIDGE, borderwidth=4, bg="red")
+button_openfile.grid(row=2, column=0, padx=430, pady=35)
+
+label_openfile = Label(main, text = "Open a File ☝", relief = FLAT, font = ("Rockwell", 15, "bold"))
+label_openfile.grid(row=3, column=0, padx=430)
 
 main.mainloop()
