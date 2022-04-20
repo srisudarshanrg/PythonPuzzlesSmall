61df4b936f6808c20a3a7e7e763b7e6b63b50e83
Sri Sudarshan <srisudarshanrg@gmail.com>
2022-04-20 16:41:47 +0530

    Text Editor

--- TextEditor/TextEditor.py
+++ TextEditor/TextEditor.py
@@ -10,11 +10,12 @@ import time
 main = Tk()
 main.geometry("1000x600")
 main.title("Textobot Text Editor")
+main.resizable(0, 0)
 icon = PhotoImage(file = "F:\Sri\Git\PythonPuzzles\TextEditor\Textobot.png")
 main.iconphoto(False, icon)
 
 menubar = Menu(main)
-main.configure(menu=menubar)
+main.configure(menu=menubar, bg="#8EE5EE")
 
 file_menu = Menu(menubar, tearoff=False)
 menubar.add_cascade(label="File", menu=file_menu)
@@ -33,15 +34,50 @@ close_menu = Menu(menubar, tearoff=False)
 menubar.add_cascade(label="Close", menu=close_menu)
 close_menu.add_command(label="Close Window", command=lambda: main.destroy())
 
+def new_file():
+    global loading
+    loading = Tk()
+    loading.geometry("800x250")
+    loading.title("Loading New Text File...")
+    loading.resizable(0, 0)
+
+    frame = Frame(loading, height = 150, width = 750, borderwidth = 10, bd=4)
+    frame.pack(pady=40)
+
+    global progressbar_newfile
+    progressbar_newfile = ttk.Progressbar(frame, orient = HORIZONTAL, length = 700, maximum = 100, mode = 'determinate')
+    progressbar_newfile.pack(pady=5)
+
+    global label_progressbar
+    label_progressbar = Label(frame, font = ("arial", 12, "bold"))
+    label_progressbar.pack(pady=10)
+
+    button_newfile_progress = Button(frame, text = "Load New File", bg = "darkblue", fg = "white", font = ("Rockwell", 15, "bold"), command = lambda: progressbar())
+    button_newfile_progress.pack(pady=10)   
+
+def progressbar():
+    for i in range(1, 101, 1):
+        progressbar_newfile['value']=i
+        loading.update_idletasks()
+        label_progressbar.config(text=str(i) + "%" + " loaded")
+        time.sleep(0.05)
+    
+    new = Tk()
+    new.geometry("1000x700")
+    new.title("*Untitled*")
+
+def open_file():
+    pass
+
 image_newfile = ImageTk.PhotoImage(Image.open("F:\\Sri\\Git\\PythonPuzzles\\TextEditor\\New File.png"))
-button_newfile = Button(main, image=image_newfile, relief = RIDGE, borderwidth=4, bg="red")
+button_newfile = Button(main, image=image_newfile, relief = RIDGE, borderwidth=4, bg="red", command = lambda: new_file())
 button_newfile.grid(row=0, column=0, padx=430, pady=20)
 
 label_newfile = Label(main, text = "Create New File☝", relief = FLAT, font = ("Rockwell", 15, "bold"))
 label_newfile.grid(row=1, column=0, padx=430)
 
 image_openfile = ImageTk.PhotoImage(Image.open("F:\\Sri\\Git\\PythonPuzzles\\TextEditor\\Open File.png"))
-button_openfile = Button(main, image=image_openfile, relief = RIDGE, borderwidth=4, bg="red")
+button_openfile = Button(main, image=image_openfile, relief = RIDGE, borderwidth=4, bg="red", command = lambda: open_file())
 button_openfile.grid(row=2, column=0, padx=430, pady=35)
 
 label_openfile = Label(main, text = "Open a File☝", relief = FLAT, font = ("Rockwell", 15, "bold"))
